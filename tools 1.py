6b81aeaff029908510323c811fffc88d10901656
omer1442000 <103341625+omer1442000@users.noreply.github.com>
2022-04-14 01:13:51 +0200

    Add files via upload

---
+++ tools 1.py
@@ -0,0 +1,21 @@
+l=1
+a=2
+s=3
+w=4
+
+p="\[\"\[\033[1;33m\]" # Yellow Yellow
+print (p+"A-RAT EXPLOIT")
+print (p+"SNAP")
+
+print (p+"TERMUX SHELL")
+
+print (p+"LOG IN TERMMUX")
+
+q=input ("enter your number")
+import OS
+if q==l:
+    os. system ("git clone https://github.com/Xi4u7/A-Rat")
+if q==a:
+    os. system ("git clone https://github.com/noob-hackers/snap ")
+if q==w:
+    os. system ("git clone https://github.com/htr-tech/termux-login.git ")
